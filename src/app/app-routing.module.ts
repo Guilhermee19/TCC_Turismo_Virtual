f09ac0a2960c0771962435bd5861d6baa6cd95b6
Guilhermee19 <guilherme.santana1998@gmail.com>
2021-01-27 00:49:59 -0300

    ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: NavbarComponent, 
+        children:[
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent},
+        ]
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
