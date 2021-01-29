27d44bee703e962b7b20a74085592130b0523982
Guilhermee19 <guilherme.santana1998@gmail.com>
2021-01-28 21:56:20 -0300

    CineBigodee atualizado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    structor() { }
 
-  urlVideo;
+    urlVideo;
 
-  ngOnInit(): void {
-    this.urlVideo='https://r1---sn-gpv7enek.c.drive.google.com/videoplayback?expire=1611739313&ei=cfgQYML7NpmYhgbbzqHwCw&ip=168.0.36.0&cp=QVRGU0hfWFNPRFhPOlc5Y0R2NFRHUDZJSGg1VFQ4YXo2d1hCeUxpRnZWU2tkdXctTUlwVDhCZVg&id=689d19f4fa668506&itag=18&source=webdrive&requiressl=yes&mh=NR&mm=32&mn=sn-gpv7enek&ms=su&mv=u&mvi=1&pl=24&sc=yes&ttl=transient&susc=dr&driveid=1MfI4vAfX6YDX5_m-PsJKHxT0EWHKv6Hv&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=46.718&lmt=1611027966603406&mt=1611724528&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRgIhALul5o4q1B79KO-ARJVDBH7HYpMMY8txyH8eTtV1Z-9MAiEAz9FtdDKFPOZK5ygfmlAqOKu-RPc2oLlLLYeVswPc6NM=&lsparams=mh,mm,mn,ms,mv,mvi,pl,sc&lsig=AG3C_xAwRQIgKaMaMx6eZkeIjc-hfvZ9bOjOSTEbOCAYHDyZUGyq8TECIQCAsH1WHOoC44sex_6UIIWuF2soYXK1r8xNGhFmElCWLA==&cpn=fCqEDHx0T9XdGykf&c=WEB_EMBEDDED_PLAYER&cver=20210126';
-  }
+    array_list = [
+        {id: 0, name: 'Mulher Maravilha',    img: 'https://previews.dropbox.com/p/thumb/ABAWPWynaYOmmb8fJ5czWFi1RepnYAXYUy_iImveG1K642GVpfAIg-RyprdJiHzWnchlzlIAEI4KBBAU8gXB9z0sX6Lc4FTdOfgvwSBMZ1IBI2vutS2qOMhx16HgPdppN60gRZRmSzzejw064-OzEkl3CCHG631BTwkGTcLF4xV6VifXSbEL0hDTo7wryuQsyZItyB6rp14ef2Q94zQ5lNsmQnvApXRc4XZauHhCdgJJYMMIZnZ3aTZP4VMOrSlu87UV8lO8VNLGsuwcVoGFdXjVL0MhF4dFyuBg-pQkX4JV_jKA6c9vPxmOQPHYP-E-M6WB3VaCusYZIVsLrUui1Dr7nz-ri1ODoUokxCCR6CTDuw/p.jpeg?fv_content=true&size_mode=5', url: 'https://mega.nz/embed/MmI3XIZR#VwgjLdG9pkvJ89ucIvr-etpOj4CDp3EiWXMlIf1ifFs'},
+        {id: 1, name: 'Vingadores Guerra infinita', img: 'https://previews.dropbox.com/p/thumb/ABAkyR5hQY05xjyrK37t9pjSQLv4b51rOA970AuN48TST5St46Gbk7wOr92At2ih0cqdDjX9Rcu-k-7INaJhvGFTtEd1lA7MEdqPVriuaL-4FJccIFnXFFaNegiv1h_qPxfM_i3IEvFP43_fLMiN_ewTTyw5wOQ4HxT2P7UCdL5N2pMwlegdJqqAohz6sVJfDblkxVePSf4NiWfNtPYl8u4WbXWD-C7YYIZkAlBikpYft9tRz-dQSeoYDAtFvpF8MnKMZi1eES1iDw8QiiLBIHx0u7hxdW8LA3AxdKiJ3Pac91GknzaJwvQSOgcBNV5CJOKHmNgWDB2khrulibOx9ixKy0V-t1kSJufz9a8D6lmpDA/p.jpeg?fv_content=true&size_mode=5', url: 'https://mega.nz/embed/A7JQAbyJ#Vb1PS6fsMhxABFQXWRLVpxR7Kv_5GFDy40u7eGycFTw'},
+        {id: 2, name: 'Vingadores Ultimato', img: 'https://previews.dropbox.com/p/thumb/ABAHawzqvLsRR1rJg1z3gHC1HodFq0HgGAmOgZZzXwXomo-a1H1HTa6-LyFHs3xlKejpKYhA6bAqti61ST6H-faz0bE5x6YMpgJPmpM3K9r6ZvCoGbktnCCcpyTWXraYgPXMQMz79vbTJIc2NmOrqtWdjvm5d8bqOfnriMxhZUSDGBTwf6GO0OFNBh3v51tLVr1JwilXwLheaYQeEazERnGW2fjcGKalKNSKoIkNi_DgUeNRcDYgFfaEAiMrk7DkMdC_f9RWfnxHiURgfhU6nVFVbp2xxDALzJ1Qde969p9dOkwiih3UsQfPl03vB9KmtGSi91KmXl6sLFoiYs5g-7i3gGc9hDCqdUJFhQ7OqFmsvw/p.jpeg?fv_content=true&size_mode=5', url: ''},
+        {id: 3, name: 'Vidro', img: 'https://previews.dropbox.com/p/thumb/ABAkLHhhAkLk0O4VJoJm5dzMMJJMfOWD2nYFBnTmaYEreRGq_0JM5cZD7-YQ1y-nrPl444suBrvEFln9v73N9Zsxy8S20dUqLNQ55Fl3FWOyUOkbCnLyfQlPCMw_Vo585v498-vp0YN7paeHb1TdFpkN7vlqwAovlJbJex2_bz_bUoqEJ01z4fyDAB2GpQqlNWsg9OU38TYWonCkRCqahbXcVLoD485DGbUy-BC3_Gdqnk0Rggxlsbv_k3-KTzlxi2bLrtff1qkdsstnSki7X0ugZ45r4XOFl8XtgwkIEzeNM4xpalPokbePXnu4Pubiw0n0n0FURyKCflOT4m5oLBGW-tIqxitOG8dnowyMjjpGow/p.jpeg?fv_content=true&size_mode=5', url: ''},
+    ];
+
+    ngOnInit(): void {
+        setTimeout(function(){
+            document.getElementById('loading').style.display="none";
+        }, 3200);
+    }
 
 }
+
