df7f81741cd40c5df6d43760ce6230770026c6e6
Guilhermee19 <guilherme.santana1998@gmail.com>
2021-05-01 19:14:31 -0300

    teste camera mark 2

--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-camera',
-  templateUrl: './camera.component.html',
-  styleUrls: ['./camera.component.css']
+    selector: 'app-camera',
+    templateUrl: './camera.component.html',
+    styleUrls: ['./camera.component.css']
 })
 export class CameraComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+      
+    }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-navbar',
@@ -7,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-    constructor() { }
+    constructor( private router: Router) { 
+        this.router.events.subscribe( data => {
+            this.url = this.router.url;
+        }); 
+    }
     
     showFiller = false;
-    select: string = 'home';
+
+    url;
+    select
 
     ngOnInit(): void {
     }
