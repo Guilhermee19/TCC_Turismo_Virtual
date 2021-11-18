46e8c51c66069ee42179b452f5db6e941be31eca
Guilhermee19 <guilherme.santana1998@gmail.com>
2021-11-17 22:41:30 -0300

    esconder tela de teste

--- src/app/virtual-environment/virtual-environment.component.ts
+++ src/app/virtual-environment/virtual-environment.component.ts
@@ -13,53 +13,48 @@ export class VirtualEnvironmentComponent implements OnInit {
         private deviceService: DeviceDetectorService
     ) { }
 
+ 
     @ViewChild('joy3Div', {static: true}) joy3Div;
 
-    array={x: 0, y:0, z:0}
-    save_array={x: 0, y:0, z:0}
+    array={x: 0, y:2, z:0}
+    save_array={x: 0, y:2, z:0}
     position: string;
-    sunLight: number;
-    
-    joy3IinputPosX
-    joy3InputPosY
-    joy3Direzione
-    joy3X
-    joy3Y
-    
-    component
+    angulo: number;
+  
     isMobile
-
+  
     ngOnInit(): void {
-        this.isMobile = this.deviceService.isMobile() || this.deviceService.isTablet();
+      this.isMobile = this.deviceService.isMobile() || this.deviceService.isTablet();
+      // this.isMobile = true;
     }
-
-    objectToPos(posPbjetc){
-        return `${posPbjetc.x} ${posPbjetc.y} ${posPbjetc.z}`
+  
+    ctToPos(posPbjetc){
+      return `${posPbjetc.x} ${posPbjetc.y} ${posPbjetc.z}`
     }
-
+  
     clickHandler(value){
-        switch(value){
-            case 'W': 
-                this.array.z -= 2;
-                break;
-            case 'D': 
-                this.array.x += 2;
-                break;
-            case 'S': 
-                this.array.z += 2;
-                break;
-            case 'A': 
-                this.array.x -= 2;
-                break;
-        }
-
-        let cameraRig = document.querySelector('#camera-rig')
-        cameraRig.setAttribute('animation', `property: position; from: ${this.save_array.x} ${this.save_array.y} ${this.save_array.z}; to: ${this.array.x} ${this.array.y} ${this.array.z}; dur: 200`)
-        this.save_array.x = this.array.x
-        this.save_array.y = this.array.y
-        this.save_array.z = this.array.z
-
-        console.log(this.save_array.x, this.save_array.y, this.save_array.z)
+      switch(value){
+        case 'W': 
+          this.array.z -= 2;
+          break;
+        case 'D': 
+          this.array.x += 2;
+          break;
+        case 'S': 
+          this.array.z += 2;
+          break;
+        case 'A': 
+          this.array.x -= 2;
+          break;
+      }
+  
+      let cameraRig = document.querySelector('#camera-rig')
+      cameraRig.setAttribute('animation', `property: position; from: ${this.save_array.x} ${this.save_array.y} ${this.save_array.z}; to: ${this.array.x} ${this.array.y} ${this.array.z}; dur: 200`)
+      this.save_array.x = this.array.x
+      this.save_array.y = this.array.y
+      this.save_array.z = this.array.z
+  
+      console.log(this.save_array.x, this.save_array.y, this.save_array.z)
     }
-
-}
+  }
+  
